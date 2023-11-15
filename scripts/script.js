b1cc26e7ff1bfc0ff42035c076082de6e1c8827c
MatthiasMatthys <matthias.matthys2@student.howest.be>
2023-11-15 20:47:47 +0100

    Add ratingview on submit

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
-
+var ratingValue = 0;  
 function setupButtonClickHandler() {
+
     // Get all buttons
     const buttons = document.querySelectorAll('.btn-group button');
   
@@ -11,14 +12,20 @@ function setupButtonClickHandler() {
 
         // Add 'clicked' class to the clicked button
         this.classList.add('clicked');
+        ratingValue = this.value;
       });
     });
   }
 
   function submit(){
     const element = document.getElementById('ratingdiv');
+    const thanksElement = document.getElementById('thanksdiv');
+    const spanElement = document.getElementById('ratingresult');
     if (element) {
         element.remove();
+        spanElement.innerHTML = ratingValue;
+        thanksElement.style.visibility = "visible";
+
     } else {
         console.log("Element with ID 'ratingdiv' not found");
     }
